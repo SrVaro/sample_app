0e3b2d57f54cbca62fff7d449ddfec6525d81ae6
Varo <sr.varo07@gmail.com>
2019-03-15 12:35:43 +0000

    Implementadas las acciones de actualizar, editar, mostrar y destruir de los usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User
   field :email, type: String
   field :password_digest, type: String
   field :remember_digest, type: String
+  field :admin, type: Boolean
   
   before_save { self.email = email.downcase }
  
